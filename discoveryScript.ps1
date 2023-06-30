c372535b5a0506c4b8b143b7825cbc2a2dbf1a57
rubisks <strand.tucker@gmail.com>
2023-06-29 18:03:13 -0600

    nothiing

--- discoveryScript.ps1
+++ discoveryScript.ps1
@@ -1,6 +1,13 @@
-$credential = Get-Credential
 
-
-Invoke-Command -ComputerName "cit361-lab.cit.byui.edu" -Credential $credential -ScriptBlock {
+# Discovery look at the computer names and find network IP addresses
     Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
-}
+
+    1..254 | ForEach-Object {
+        $ipAddress = "192.168.1.$_"
+        $pingResult = Test-Connection -ComputerName $ipAddress -Count 1 -Quiet
+    
+        if ($pingResult) {
+            "$ipAddress is active"
+        }
+    } 
+    
